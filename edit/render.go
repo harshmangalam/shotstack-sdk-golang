d18b0afdc771424ef3b35371f2c7784a87ca1343
harshmangalam <harshdev8218@gmail.com>
2022-09-27 18:25:50 +0530

    updated asset methods api

--- edit/render.go
+++ edit/render.go
@@ -346,35 +346,43 @@ func (c *Clip) SetTransform(transform *Transform) *Clip {
 //  asset
 
 func NewAsset() *Asset {
-	return &Asset{}
+	a := new(Asset)
+	return a
 }
 
 func (a *Asset) SetAssetType(assetType AssetType) *Asset {
-	return &Asset{Type: assetType}
+	a.Type = assetType
+	return a
 }
 
-func (a *Asset) SetVideoAsset(videoAssetType *VideoAssetType) *Asset {
-	return &Asset{VideoAsset: videoAssetType}
+func (a *Asset) SetVideoAsset(videoAsset *VideoAssetType) *Asset {
+	a.VideoAsset = videoAsset
+	return a
 }
 
 func (a *Asset) SetAudioAsset(audioAsset *AudioAssetType) *Asset {
-	return &Asset{AudioAsset: audioAsset}
+	a.AudioAsset = audioAsset
+	return a
 }
 
-func (a *Asset) SetImageAsset(imageAsser *ImageAssetType) *Asset {
-	return &Asset{ImageAsset: imageAsser}
+func (a *Asset) SetImageAsset(imageAsset *ImageAssetType) *Asset {
+	a.ImageAsset = imageAsset
+	return a
 }
 
 func (a *Asset) SetHtmlAsset(htmlAsset *HtmlAssetType) *Asset {
-	return &Asset{HtmlAsset: htmlAsset}
+	a.HtmlAsset = htmlAsset
+	return a
 }
 
 func (a *Asset) SetTitleAsset(titleAsset *TitleAssetType) *Asset {
-	return &Asset{TitleAsset: titleAsset}
+	a.TitleAsset = titleAsset
+	return a
 }
 
-func (a *Asset) SetLumaAsset(LumaAsset *LumaAssetType) *Asset {
-	return &Asset{LumaAsset: LumaAsset}
+func (a *Asset) SetLumaAsset(lumaAsset *LumaAssetType) *Asset {
+	a.LumaAsset = lumaAsset
+	return a
 }
 
 // video asset
