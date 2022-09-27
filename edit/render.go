95db5b5579f1b8a08f8d9042d9ee3a512db7ecff
harshmangalam <harshdev8218@gmail.com>
2022-09-27 18:13:10 +0530

    added html assets methods

--- edit/render.go
+++ edit/render.go
@@ -432,6 +432,41 @@ func (i *ImageAssetType) SetImageAssetCrop(crop *Crop) *ImageAssetType {
 	return &ImageAssetType{Crop: crop}
 }
 
+// html asset
+
+func NewHtmlAsset() *HtmlAssetType {
+	h := new(HtmlAssetType)
+	return h
+}
+
+func (h *HtmlAssetType) SetHtmlAssetHtml(html string) *HtmlAssetType {
+	h.Html = html
+	return h
+}
+
+func (h *HtmlAssetType) SetHtmlAssetCss(css string) *HtmlAssetType {
+	h.Css = css
+	return h
+}
+
+func (h *HtmlAssetType) SetHtmlAssetHeight(height int) *HtmlAssetType {
+
+	h.Height = height
+	return h
+}
+
+func (h *HtmlAssetType) SetHtmlAssetWidth(width int) *HtmlAssetType {
+
+	h.Width = width
+	return h
+}
+
+func (h *HtmlAssetType) SetHtmlAssetPosition(pos Position) *HtmlAssetType {
+
+	h.Position = pos
+	return h
+}
+
 // offset
 
 // transition
