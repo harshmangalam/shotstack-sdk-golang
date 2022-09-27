e85ffdddb93ab7f70b95a2515d1980cdcbe4fa51
harshmangalam <harshdev8218@gmail.com>
2022-09-27 19:06:43 +0530

    added transition methods

--- edit/transition.go
+++ edit/transition.go
@@ -30,3 +30,17 @@ type Transition struct {
 	In  TransitionType
 	Out TransitionType
 }
+
+func NewTransition() *Transition {
+	return new(Transition)
+}
+
+func (t *Transition) SetIn(in TransitionType) *Transition {
+	t.In = in
+	return t
+}
+
+func (t *Transition) SetOut(out TransitionType) *Transition {
+	t.Out = out
+	return t
+}
