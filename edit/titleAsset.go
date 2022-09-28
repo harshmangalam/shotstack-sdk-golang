900ddd9e41ec05b1effe38f020fa5f4b78ae69c6
harshmangalam <harshdev8218@gmail.com>
2022-09-29 00:07:56 +0530

    added methods

--- edit/titleAsset.go
+++ edit/titleAsset.go
@@ -16,9 +16,14 @@ const (
 )
 
 type TitleAssetType struct {
-	Type  string          `json:"type"`
-	Text  string          `json:"text"`
-	Style TitleAssetStyle `json:"style"`
+	Type       string          `json:"type"`
+	Text       string          `json:"text"`
+	Style      TitleAssetStyle `json:"style,omitempty"`
+	Color      string          `json:"color,omitempty"`
+	Size       string          `json:"size,omitempty"`
+	Background string          `json:"background,omitempty"`
+	Position   string          `json:"position,omitempty"`
+	Offset     *Offset         `json:"offset,omitempty"`
 }
 
 func NewTitleAsset() *TitleAssetType {
@@ -34,5 +39,30 @@ func (t *TitleAssetType) SetText(text string) *TitleAssetType {
 
 func (t *TitleAssetType) SetStyle(style TitleAssetStyle) *TitleAssetType {
 	t.Style = style
+	return t
+}
+
+func (t *TitleAssetType) SetColor(color string) *TitleAssetType {
+	t.Color = color
+	return t
+}
+
+func (t *TitleAssetType) SetSize(size string) *TitleAssetType {
+	t.Size = size
+	return t
+}
+
+func (t *TitleAssetType) SetBackground(background string) *TitleAssetType {
+	t.Background = background
+	return t
+}
+
+func (t *TitleAssetType) SetPosition(position string) *TitleAssetType {
+	t.Position = position
+	return t
+}
+
+func (t *TitleAssetType) SetOffset(offset *Offset) *TitleAssetType {
+	t.Offset = offset
 	return t
 }
