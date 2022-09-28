abf7e619dbf60b720348522d1f01a7035c4a2621
harshmangalam <harshdev8218@gmail.com>
2022-09-28 09:27:34 +0530

    updated html asset

--- edit/asset.go
+++ edit/asset.go
@@ -1,14 +1,5 @@
 package edit
 
-type HtmlAssetType struct {
-	Html       string
-	Css        string
-	Width      int
-	Height     int
-	Background string
-	Position   Position
-}
-
 type AudioAssetType struct {
 	Src    string
 	Trim   int
@@ -94,36 +85,4 @@ func (a *AudioAssetType) SetAudioAssetVolume(vol float32) *AudioAssetType {
 func (a *AudioAssetType) SetAudioAssetEffect(effect AudioEffect) *AudioAssetType {
 	a.Effect = effect
 	return a
-}
-
-// html asset
-
-func NewHtmlAsset() *HtmlAssetType {
-	h := new(HtmlAssetType)
-	return h
-}
-
-func (h *HtmlAssetType) SetHtmlAssetHtml(html string) *HtmlAssetType {
-	h.Html = html
-	return h
-}
-
-func (h *HtmlAssetType) SetHtmlAssetCss(css string) *HtmlAssetType {
-	h.Css = css
-	return h
-}
-
-func (h *HtmlAssetType) SetHtmlAssetHeight(height int) *HtmlAssetType {
-	h.Height = height
-	return h
-}
-
-func (h *HtmlAssetType) SetHtmlAssetWidth(width int) *HtmlAssetType {
-	h.Width = width
-	return h
-}
-
-func (h *HtmlAssetType) SetHtmlAssetPosition(pos Position) *HtmlAssetType {
-	h.Position = pos
-	return h
 }
