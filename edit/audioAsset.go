67f8c82d8a0962da243933abd4ee054424f73278
harshmangalam <harshdev8218@gmail.com>
2022-09-29 00:00:16 +0530

    updated filds

--- edit/audioAsset.go
+++ edit/audioAsset.go
@@ -1,32 +1,34 @@
 package edit
 
 type AudioAssetType struct {
+	Type   string      `json:"type"`
 	Src    string      `json:"src"`
-	Trim   int         `json:"trim"`
-	Volume float32     `json:"volume"`
-	Effect AudioEffect `json:"effect"`
+	Trim   int         `json:"trim,omitempty"`
+	Volume float32     `json:"volume,omitempty"`
+	Effect AudioEffect `json:"effect,omitempty"`
 }
 
 func NewAudioAsset() *AudioAssetType {
 	a := new(AudioAssetType)
+	a.Type = "audio"
 	return a
 }
 
-func (a *AudioAssetType) SetAudioAssetSrc(src string) *AudioAssetType {
+func (a *AudioAssetType) SetSrc(src string) *AudioAssetType {
 	a.Src = src
 	return a
 }
-func (a *AudioAssetType) SetAudioAssetTrim(trim int) *AudioAssetType {
+func (a *AudioAssetType) SetTrim(trim int) *AudioAssetType {
 	a.Trim = trim
 	return a
 }
 
-func (a *AudioAssetType) SetAudioAssetVolume(vol float32) *AudioAssetType {
+func (a *AudioAssetType) SetVolume(vol float32) *AudioAssetType {
 	a.Volume = vol
 	return a
 }
 
-func (a *AudioAssetType) SetAudioAssetEffect(effect AudioEffect) *AudioAssetType {
+func (a *AudioAssetType) SetEffect(effect AudioEffect) *AudioAssetType {
 	a.Effect = effect
 	return a
 }
