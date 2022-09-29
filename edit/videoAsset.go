8490788810a45407c89b7679b23183227624a294
harshmangalam <harshdev8218@gmail.com>
2022-09-29 09:28:36 +0530

    updated video asset methods

--- edit/videoAsset.go
+++ edit/videoAsset.go
@@ -13,22 +13,22 @@ func NewVideoAsset() *VideoAsset {
 	v.Type = "video"
 	return v
 }
-func (v *VideoAsset) SetVideoAssetSrc(src string) *VideoAsset {
+func (v *VideoAsset) SetSrc(src string) *VideoAsset {
 	v.Src = src
 	return v
 }
 
-func (v *VideoAsset) SetVideoAssetTrim(trim int) *VideoAsset {
+func (v *VideoAsset) SetTrim(trim int) *VideoAsset {
 	v.Trim = trim
 	return v
 }
 
-func (v *VideoAsset) SetVideoAssetVolume(vol float32) *VideoAsset {
+func (v *VideoAsset) SetVolume(vol float32) *VideoAsset {
 	v.Volume = vol
 	return v
 }
 
-func (v *VideoAsset) SetVideoAssetCrop(crop *Crop) *VideoAsset {
+func (v *VideoAsset) SetCrop(crop *Crop) *VideoAsset {
 	v.Crop = crop
 	return v
 }
